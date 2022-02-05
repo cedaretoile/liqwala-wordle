b0cf233c0e2788cd153b843c9fbb68ef0bdfa48b
cedaretoile <cedartaatawasi@gmail.com>
2022-02-04 20:03:57 -0800

    title content

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,12 +1,11 @@
 export const CONFIG = {
   tries: 6, // This changes how many tries you get to finish the wordle
-  language: 'ditidaqiic̓aq cicqiʔ', // This changes the display name for your language
+  language: 'Liq̓ʷala', // This changes the display name for your language
   wordLength: 5, // This sets how long each word is based on how many characters (as defined in orthography.ts) are in each word
   author: 'Cedar Étoile', // with help from Ben Altair
   authorWebsite: 'https://tlaahaqsak.com', // Put a link to your website or social media here
-  wordListSource: 't̓aat̓aaqsapa', // Describe the source material for your words here
-  wordListSourceLink:
-    'http://www.hesquiahtlanguage.org/uploads/6/8/7/0/6870919/taataaqsapa_1991_ocr.pdf', // Put a link to the source material for your words here
+  wordListSource: 'many sources', // Describe the source material for your words here
+  wordListSourceLink: '', // Put a link to the source material for your words here
   //
   // THESE NEXT SETTINGS ARE FOR ADVANCED USERS
   //
